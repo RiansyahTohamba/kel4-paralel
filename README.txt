40274dd768f1a781277400c6c7b73bb2b9f0efdb
muh riansyah <riansyah@41studio.com>
2019-02-27 17:51:11 +0700

    add multiple matrix code

---
+++ README.txt
@@ -0,0 +1,3 @@
+contoh run
+mpiexec -n 4 ./hello
+4 untuk 4 processor, hello nama file yang udah di compile
--- topic_1.c
+++ topic_1.c
@@ -3,10 +3,13 @@
 #include <stdio.h>
 
 main(){
-	multiple_(5, 12,100,10.5);
+	printf("==== result with normal matrix multiplication ===");
+	multiple_vector_matrix(5, 12,100,10.5);
+	printf("==== result with algorithm ==== ");
+	multiple_vector_matrix(5, 12,100,10.5);
 }
 
-int multiple_(int n, int p,int max_it, float tol)
+int multiple_vector_matrix(int n, int p,int max_it, float tol)
 {
 
 }
