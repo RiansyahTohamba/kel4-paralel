5c6428d8586dd19445003a399d52396498e30a90
kartuhitam <destiana14@mhs.if.its.ac.id>
2019-03-18 21:14:50 +0700

    Add time

--- jacobi/jacobi_scatter.c
+++ jacobi/jacobi_scatter.c
@@ -14,17 +14,16 @@ int main(int argc, char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     char processor_name[MPI_MAX_PROCESSOR_NAME];
-    MPI_Request request;
     int name_len;
     int sum_one_row = 0;
-    int operation_flag = 1;
     MPI_Get_processor_name(processor_name, &name_len);
 
     int fixedsize;
-    //double start, finish;
+    double start, finish;
+    struct timespec begin, end;
 
     double tol = 0.001;
-    double distance, total_xrow;
+    double distance, total_xrow, total_time = 0.0, run_time = 0.0;
 
     int matrix[matrix_size][matrix_size], b[matrix_size], recvbuff[(matrix_size * (matrix_size/numtasks))];
     double x[matrix_size], x_record[matrix_size][1001], v_result[matrix_size];
@@ -32,6 +31,7 @@ int main(int argc, char** argv){
     int belowtol = 0;
     
     if(rank == 0){
+        clock_gettime(CLOCK_REALTIME, &begin);
         //generate random val
         for(i = 0; i < matrix_size; i++){
             sum_one_row = 0;
@@ -45,6 +45,7 @@ int main(int argc, char** argv){
             x_record[i][0] = 0.0;
 
         }
+        // Debugging
         /*printf("\nMatrix: \n");
         for(i = 0; i < matrix_size; i++){
             for(j = 0; j < matrix_size; j++){
@@ -52,15 +53,16 @@ int main(int argc, char** argv){
             }
             printf("\n");
         }
-        //start = MPI_Wtime();
-        printf("\n\nB: %d, %d, %d\n\n", b[0], b[1], b[2]);
-        //printf("\n\nB: %d, %d, %d, %d, %d\n\n", b[0], b[1], b[2], b[3], b[4]);*/
+        printf("\nB:");
+        for(i = 0; i < matrix_size; i++) printf(" %d,\n\n", b[i]);
+        printf("\n");*/
     }
     fixedsize = matrix_size/numtasks;
+    if(rank == 0) start = MPI_Wtime();
     MPI_Bcast(&x, matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Scatter(&matrix, fixedsize * matrix_size, MPI_INT, &recvbuff, (matrix_size * (matrix_size/numtasks)), MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Scatter(&b, fixedsize, MPI_INT, &vecbuff, matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
-
+    if(rank == 0){ finish = MPI_Wtime(); total_time = finish - start;}
 
     for(i = 0; i < 1000; i++){
         //Jacobi Method
@@ -69,20 +71,21 @@ int main(int argc, char** argv){
             for(k = 0; k < matrix_size; k++){
                 if((j * matrix_size) + k != ((j * matrix_size) + (rank * fixedsize) + j))
                 total_xrow += (double) recvbuff[(j * matrix_size) + k] * x[k];
-                //printf("%d Value: %d\n", (j * matrix_size) + k, recvbuff[(j * matrix_size) + k]);
             }
             v_result[j] = (double) (vecbuff[j] - total_xrow) / recvbuff[(j * matrix_size) + (rank * fixedsize) + j];
-            //printf("%d - %f / %d = %f\n", vecbuff[j], total_xrow, recvbuff[(j * matrix_size) + (rank * fixedsize) + j], v_result[j]);
-            //printf("lol %d: from %d & %d then %f\n", ((j * matrix_size) + (rank * fixedsize) + j), recvbuff[(j * matrix_size) + (rank * fixedsize) + j], vecbuff[i], v_result[i]);
         }
+        if(rank == 0) start = MPI_Wtime();
         MPI_Allgather(&v_result, fixedsize, MPI_DOUBLE, &x, fixedsize, MPI_DOUBLE, MPI_COMM_WORLD);
         MPI_Barrier(MPI_COMM_WORLD);
         if(rank == 0){
+            finish = MPI_Wtime(); 
+            total_time = finish - start;
+            //print x result
             /*for(j = 0; j < matrix_size; j++){
                 x_record[j][i+1] = x[j];
                 printf("%f, ", x_record[j][i+1]);
-            }*/
-            printf("\n");
+            }
+            printf("\n");*/
             for(k = 0; k < matrix_size; k++)
                 if(fabs(x_record[k][i] - x[k]) < tol) belowtol += 1;
         }
@@ -91,9 +94,12 @@ int main(int argc, char** argv){
         else belowtol = 0;
     }
     if(rank == 0){
+        clock_gettime(CLOCK_REALTIME, &end);
+        run_time = (end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec) / 1000000000.0;
         printf("Final Result:");
         for(i = 0; i < matrix_size; i++) printf(" %f,", x[i]);
-        printf("\n");
+        printf("\nCommunication Time: %f\n", total_time);
+        printf("Running Time: %f\n", run_time);
     }
     MPI_Finalize();
 }
