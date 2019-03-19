4c6bc71344d96df2c5bf8a76829f7224ebaf4da1
kartuhitam <destiana14@mhs.if.its.ac.id>
2019-03-19 08:48:27 +0700

    Fix Jacobi

--- jacobi/jacobi_scatter.c
+++ jacobi/jacobi_scatter.c
@@ -26,10 +26,12 @@ int main(int argc, char** argv){
     double distance, total_xrow, total_time = 0.0, run_time = 0.0;
 
     int matrix[matrix_size][matrix_size], b[matrix_size], recvbuff[(matrix_size * (matrix_size/numtasks))];
-    double x[matrix_size], x_record[matrix_size][1001], v_result[matrix_size];
+    double x[matrix_size], x_past[matrix_size], x_record[matrix_size][1001], v_result[matrix_size];
     int i, j, k, vecbuff[(matrix_size/numtasks)];
     int belowtol = 0;
     
+    for(i = 0; i < matrix_size; i++) x_past[i] = 0.0;
+
     if(rank == 0){
         clock_gettime(CLOCK_REALTIME, &begin);
         //generate random val
@@ -57,12 +59,14 @@ int main(int argc, char** argv){
         for(i = 0; i < matrix_size; i++) printf(" %d,\n\n", b[i]);
         printf("\n");*/
     }
+
     fixedsize = matrix_size/numtasks;
     if(rank == 0) start = MPI_Wtime();
     MPI_Bcast(&x, matrix_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Scatter(&matrix, fixedsize * matrix_size, MPI_INT, &recvbuff, (matrix_size * (matrix_size/numtasks)), MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Scatter(&b, fixedsize, MPI_INT, &vecbuff, matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(&b, fixedsize, MPI_INT, &vecbuff, fixedsize, MPI_INT, 0, MPI_COMM_WORLD);
     if(rank == 0){ finish = MPI_Wtime(); total_time = finish - start;}
+    MPI_Barrier(MPI_COMM_WORLD);
 
     for(i = 0; i < 1000; i++){
         //Jacobi Method
@@ -76,22 +80,23 @@ int main(int argc, char** argv){
         }
         if(rank == 0) start = MPI_Wtime();
         MPI_Allgather(&v_result, fixedsize, MPI_DOUBLE, &x, fixedsize, MPI_DOUBLE, MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
         if(rank == 0){
             finish = MPI_Wtime(); 
             total_time = finish - start;
+        }
             //print x result
             /*for(j = 0; j < matrix_size; j++){
                 x_record[j][i+1] = x[j];
                 printf("%f, ", x_record[j][i+1]);
             }
             printf("\n");*/
-            for(k = 0; k < matrix_size; k++)
-                if(fabs(x_record[k][i] - x[k]) < tol) belowtol += 1;
-        }
-        MPI_Bcast(&belowtol, matrix_size, MPI_INT, 0, MPI_COMM_WORLD);
+        for(k = 0; k < matrix_size; k++)
+            if(fabs(x_past[k] - x[k]) < tol) belowtol += 1;
         if(belowtol == matrix_size) break;
-        else belowtol = 0;
+        else{
+            belowtol = 0;
+            for(k = 0; k < matrix_size; k++) x_past[k] = x[k];
+        }
     }
     if(rank == 0){
         clock_gettime(CLOCK_REALTIME, &end);
@@ -103,3 +108,4 @@ int main(int argc, char** argv){
     }
     MPI_Finalize();
 }
+
